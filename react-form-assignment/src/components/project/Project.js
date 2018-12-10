7472f44ddc8b3f25bd5b24e8d755f70c91fb700f
soumya.ghosh <ghoshsoumya001@gmail.com>
2018-12-10 13:33:18 +0530

    project info added

--- react-form-assignment/src/components/project/Project.js
+++ react-form-assignment/src/components/project/Project.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import './Project.css';
 import ProjectHeader from './project-header/ProjectHeader';
-
+import ProjectInfo from './project-info/ProjectInfo';
 class Project extends Component {
+    isProjectInfoEnabled = false;
+    isProjectSharingEnabled = false
+    constructor(){
+        super();
+        this.state = {
+            isSubmitEnabled: this.isProjectInfoEnabled && this.isProjectSharingEnabled
+        }
+    }
+    setProjectInfoStatus = (isEnabled )=>{
+        this.isProjectInfoEnabled = isEnabled;
+    }
+    setProjectInfoStatus = (isEnabled )=>{
+        this.isProjectSharingEnabled = isEnabled;
+    }
     render() {
-        return(
-            <div className="project">
-             <ProjectHeader></ProjectHeader>
-            </div>
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <div className="project">
+                    <ProjectHeader></ProjectHeader>
+                    <ProjectInfo></ProjectInfo>
+                </div>
+                <input type="submit" disabled = {!this.state.isSubmitEnabled} value="Submit"/>
+            </form>
 
         )
     }
